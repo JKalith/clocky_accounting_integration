e941d72e5625d34b0811a87fad1e5d58df3102f1
JKalith <Jamesarias2383@gmail.com>
2025-10-23 12:30:14 -0600

    Fix simplified code

--- models/account_move_line_cabys.py
+++ models/account_move_line_cabys.py
@@ -4,5 +4,9 @@ from odoo import models, fields
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    # Solo mostramos el CABYS existente (no lo creamos)
-    cabys = fields.Char(related="product_id.product_tmpl_id.x_studio_cabys", store=True, readonly=True)
+    cabys = fields.Char(
+        string="CABYS",
+        related="product_id.x_studio_cabys",
+        store=True,
+        readonly=True,
+    )
