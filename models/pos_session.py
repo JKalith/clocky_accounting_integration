0097891dd790435e702038e327395c774f5f5d20
JKalith <Jamesarias2383@gmail.com>
2025-11-10 15:22:19 -0600

    Update POS partner activity field in client details
    Refactored the XML template to use the correct class selector for the client details panel and simplified the markup. Removed unnecessary comments and docstrings from the Python model to streamline code.

--- models/pos_session.py
+++ models/pos_session.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# models/pos_session.py
 
 from odoo import models
 
@@ -6,10 +6,6 @@ class PosSession(models.Model):
     _inherit = "pos.session"
 
     def _loader_params_res_partner(self):
-        """
-        Añadimos 'codigo_actividad_receptor' a los campos que
-        el POS carga del modelo res.partner.
-        """
         params = super()._loader_params_res_partner()
         fields = params["search_params"]["fields"]
         if "codigo_actividad_receptor" not in fields:
